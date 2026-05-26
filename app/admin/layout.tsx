b5d3a0048ee850b455809da2bc99ce1a523ecde5
underscoreX4static <drice.mkd@gmail.com>
2026-05-26 10:13:43 +1000

    Mobile: toggle dark mode bouton fixe top-right

--- app/admin/layout.tsx
+++ app/admin/layout.tsx
@@ -1,11 +1,13 @@
 import Sidebar from '@/components/admin/Sidebar'
 import ThemeProvider from '@/components/admin/ThemeProvider'
+import ThemeToggle from '@/components/admin/ThemeToggle'
 
 export default function AdminLayout({ children }: { children: React.ReactNode }) {
   return (
     <ThemeProvider>
       <div className="min-h-screen bg-gray-50 dark:bg-gray-950 flex flex-col md:flex-row">
         <Sidebar />
+        <ThemeToggle />
         <main className="flex-1 overflow-y-auto p-4 md:p-6 pb-24 md:pb-6">
           {children}
         </main>
